0bda95775cbe63278e14d399d5f143b48419ad64
Natalya Lobanova <knopka40a@mail.ru>
2022-08-16 15:53:18 +0500

    Add Ex019

---
+++ Ex019/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+int number = ReadInt("Введите пятизначное число: ");
+
+if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
