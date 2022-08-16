b18f7ec0cf33abf11d81ff5e6d95f9d2a1005b9c
Natalya Lobanova <knopka40a@mail.ru>
2022-08-16 15:55:37 +0500

    Add Ex023

---
+++ Ex023/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+int randomNumber = new Random().Next(1, 10);
+Console.WriteLine(randomNumber);
+int count = 1;
+
+while(count <= randomNumber)
+{
+    Console.Write(Cube(count) + " ");
+    count++;
+}
+
+int Cube(int num)
+{
+    return(num * num * num);
+}
+
+
