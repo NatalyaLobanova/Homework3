babd13a5adc627eddb4ac9c2a6f7caa290fa363e
Natalya Lobanova <knopka40a@mail.ru>
2022-08-16 15:55:03 +0500

    Add Ex021

---
+++ Ex021/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+int x1 = 7;
+int y1 = -5; 
+int z1 = 0;
+int x2 = 1;
+int y2 = -1;
+int z2 = 9;
+
+int lengthX = x2 - x1;
+int lengthY = y2 - y1;
+int lrngthZ = z2 - z1;
+double distance = Math.Sqrt(lengthX*lengthX + lengthY*lengthY + lrngthZ*lrngthZ);
+Console.WriteLine($"A({x1},{y1},{z1}); B({x2},{y2},{z2}) -> {distance}");
